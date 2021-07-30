9cbcd522989b003b6075c8fa69270bace4e0b903
jason <2B3>
2021-07-30 14:45:09 -0400

    commit 104B: Turned ErrorModal into a multi-comp file

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -3,24 +3,30 @@ import ReactDOM from "react-dom";
 import Card from "./Card";
 import Button from "./Button";
 import classes from "./ErrorModal.module.css";
-
+const Backdrop = function (props) {
+  return <div className={classes.backdrop} onClick={props.onConfirm} />;
+};
+const Modal = function (props) {
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={props.onConfirm}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
 const ErrorModal = (props) => {
   return ReactDOM.createPortal(
     <React.Fragment>
-      <div className={classes.backdrop} onClick={props.onConfirm} />
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button onClick={props.onConfirm}>Okay</Button>
-        </footer>
-      </Card>
-    </React.Fragment>,
-    document.getElementById("modal-root")
+      {Backdrop(props)} 
+      {Modal(props)}
+    </React.Fragment>,document.getElementById("modal-root")
   );
 };
 
